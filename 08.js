0bb9ba014450ed26ff75486974b77a0b131320f9
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2021-11-09 08:50:38 +0200

    Init

---
+++ 08.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myName = "my name is Roma ";
+const myStr ="Hi, " + myName + "and I am well";
---
+++ 09.js
@@ -0,0 +1,4 @@
+// Change code below this line
+const someAdjective = "a very great idea for a growing up like a profi!";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
